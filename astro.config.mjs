84e8805199c97b8ce78be006ed73b2e7c3356b46
Tulio Gerbassi <tuliogerbassi@gmail.com>
2025-04-29 07:48:27 +0800

    Remove Sanity integration and related code; update ContactForm to use FormSubmit for handling submissions. Add thank-you page for form confirmation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import sanity from "@sanity/astro";
 // import { sanity } from "@astrojs/sanity";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
-    // sanity({
-    //   projectId: "6edws5st",
-    //   dataset: "production",
-    //   useCdn: false,
-    // }),
+  
   ],
   vite: {
     plugins: [tailwindcss()],
