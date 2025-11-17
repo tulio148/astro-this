5c6a9d26bb09b6fb9e9d36169adc5cc1e600721a
tulio <tuliogerbassi@gmail.com>
2025-11-17 12:30:38 +0800

    Update class schedule and time for "Samba no Pe Beginners"

--- src/data/terms.ts
+++ src/data/terms.ts
@@ -14,14 +14,14 @@ export const terms: Term[] = [
   {
     name: "Samba no Pe Beginners",
     level: "Beginner",
-    dateBegin: "2nd October 2025",
-    dateEnd: "31st October 2025",
-    time: "7:40pm - 8:40pm",
+    dateBegin: "8th January 2026",
+    dateEnd: "5th February 2026",
+    time: "7:40pm - 8:30pm",
     location: "Balcatta",
     price: "$110",
     bookingLink:
       "https://checkout.square.site/merchant/G3RV3Q4J8CFQJ/checkout/Q3CUDWNQV6I4NRFX34LZGM2K?src=sheet",
     description:
-      "Step into the vibrant world of Brazilian samba with our fun 5-week beginner course! Learn the samba basic step, body movement, technique, and choreography in a supportive setting. Perfect for complete beginners or anyone wanting to refresh their fundamentals. Let’s dance Samba no Pé—just like in Rio’s Carnival!",
+      "Step into the vibrant world of Brazilian samba with our fun 5-week beginner course! Learn the samba basic step, body movement, technique, and choreography in a supportive setting. Perfect for complete beginners or anyone wanting to refresh their fundamentals. Let's dance Samba no Pé—just like in Rio's Carnival!",
   },
 ];
