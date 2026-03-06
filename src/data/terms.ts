9e3dc357abd89239bc6826f0d1ea653106e2893f
Tulio Gerbassi <tuliogerbassi@gmail.com>
2026-03-06 21:15:25 +0800

    Update level for "Samba no Pe Beginners/Intermediate" to "Beginner/Intermediate"

--- src/data/terms.ts
+++ src/data/terms.ts
@@ -13,7 +13,7 @@ export interface Term {
 export const terms: Term[] = [
   {
     name: "Samba no Pe Beginners/Intermediate",
-    level: "Beginner",
+    level: "Beginner/Intermediate",
     dateBegin: "11th March 2026",
     dateEnd: "8th April 2026",
     time: "7:00pm - 8:00pm",
@@ -23,5 +23,18 @@ export const terms: Term[] = [
       "https://checkout.square.site/merchant/G3RV3Q4J8CFQJ/checkout/Q3CUDWNQV6I4NRFX34LZGM2K?src=sheet",
     description:
       "Step into the vibrant world of Brazilian samba with our fun 5-week course designed for both beginners and those with some experience! You'll learn (or refine) the samba basic step, body movement, technique, and choreography in a supportive, welcoming setting. Perfect for complete beginners ready to start their samba journey, as well as dancers wanting to strengthen and polish their fundamentals. Let's dance Samba no Pé - just like in Rio's Carnival!",
+  },
+  {
+    name: "Samba no Pé Beginners/Intermediate",
+    level: "Beginner/Intermediate",
+    dateBegin: "22nd April 2026",
+    dateEnd: "20th May 2026",
+    time: "7:00pm - 8:00pm",
+    location: "Balcatta",
+    price: "$110",
+    bookingLink:
+      "https://checkout.square.site/merchant/G3RV3Q4J8CFQJ/checkout/Q3CUDWNQV6I4NRFX34LZGM2K?src=sheet",
+    description:
+      "Step into the vibrant world of Brazilian samba with our fun 5-week course designed for both beginners and those with some experience! You’ll learn (or refine) the samba basic step, body movement, technique, and choreography in a supportive, welcoming setting. Perfect for complete beginners ready to start their samba journey, as well as dancers wanting to strengthen and polish their fundamentals. Let’s dance Samba no Pé - just like in Rio’s Carnival!",
   },
 ];
